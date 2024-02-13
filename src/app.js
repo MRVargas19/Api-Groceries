9d6ce048f0c21d31d0f138e9e84cfcc3dff86a93
Martin Vargas <831martingutierrez@gmail.com>
2024-02-13 16:31:09 -0600

    Mi primer repo

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import express  from "express";
+import morgan from "morgan";
+import ProductsRouters from './routes/products.routes.js'
+config();
+const app=express();
+
+app.set('view engine' , 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(morgan('dev'));
+app.use('/api/products',ProductsRouters);
+
+export default app;
---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,43 @@
+import productDAO from "../dao/products.dao.js";
+export const getAll=(req,res)=>{
+    productDAO.getAll()
+    .then(products=> res.render('../src/views/index',{products}))
+    .catch(err=> res.json({
+        status:"server unavailable"
+    }));
+   
+};
+
+export const getOne=(req,res)=>{
+    productDAO.getOne(req.params.barcode)
+    .then(product=> {
+
+        !product ? res.json({
+            message: "Server unavailable"
+        }) :res.render('../src/views/edit',{product})
+    })
+    .catch(err=>res.json({
+        status:"server unavilable"
+    }))
+} ;
+
+export const insertOne=(req,res)=>{
+    const product = req.body
+    console.log(product)
+    productDAO.insertOne(req.body)
+    .then(result=>res.redirect('/api/products/'))
+    .catch(err=>res.json({status:"server unavailable"}));
+};
+
+export const deleteOne=(req,res)=>{
+    productDAO.deleteOne(req.params.barcode)
+    .then(result=>res.redirect('/api/products/'))
+    .catch(err=>res.json({status: "Server unavaliable =/"}));
+}
+export const updateOne=(req,res)=>{
+    console.log(req.body)
+    productDAO.updateOne(req.params.barcode, req.body)
+    .then(result=>res.redirect('/api/products/'))
+    .catch(err=>res.json({status: "Server unavaliable =/"}));
+}
+
